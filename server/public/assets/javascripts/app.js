c065671077e41269bda904d2a7a783227b6154d7
Connor Hindley <conn.hindley@gmail.com>
2014-07-08 15:37:32 -0400

    tweak styles

--- server/public/assets/javascripts/app.js
+++ server/public/assets/javascripts/app.js
@@ -46,6 +46,39 @@
 (function() {
     'use strict';
 
+    angular.module('moonunit.ui.directives', [])
+        .directive('mainNav', function() {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'components/UI/main-nav.html',
+                controller: function($scope) {
+                    $scope.navItems = [{
+                        title: 'Dashboard',
+                        route: 'dashboard',
+                        icon: 'dashboard',
+                    }, {
+                        title: 'Users',
+                        icon: 'users',
+                        route: 'users'
+                    }, {
+                        title: 'Smoke Builds',
+                        icon: 'cloud-download',
+                        route: 'smoke-builds'
+                    }];
+                }
+            };
+        })
+        .directive('loading', function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'components/UI/loading.html'
+            };
+        });
+})();
+(function() {
+    'use strict';
+
     angular.module('moonunit.testResults.controllers', ['moonunit.testResults.data', 'ngGrid'])
         .controller('ListTestResultsCtrl', function($scope, TestResults) {
             $scope.loading = true;
@@ -129,37 +162,4 @@
             }
         ]);
 
-})();
-(function() {
-    'use strict';
-
-    angular.module('moonunit.ui.directives', [])
-        .directive('mainNav', function() {
-            return {
-                restrict: 'E',
-                replace: true,
-                templateUrl: 'components/UI/main-nav.html',
-                controller: function($scope) {
-                    $scope.navItems = [{
-                        title: 'Dashboard',
-                        route: 'dashboard',
-                        icon: 'dashboard',
-                    }, {
-                        title: 'Users',
-                        icon: 'users',
-                        route: 'users'
-                    }, {
-                        title: 'Smoke Builds',
-                        icon: 'cloud-download',
-                        route: 'smoke-builds'
-                    }];
-                }
-            };
-        })
-        .directive('loading', function() {
-            return {
-                restrict: 'E',
-                templateUrl: 'components/UI/loading.html'
-            };
-        });
 })();
