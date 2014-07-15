92adea18b6830b61d364546a83792e669693b430
Jerrod <jllankfo@ncsu.edu>
2014-07-15 17:28:03 -0400

    add url when getting a users builds

--- server/app/serializers/user_serializer.rb
+++ server/app/serializers/user_serializer.rb
@@ -5,7 +5,7 @@ class UserSerializer < ActiveModel::Serializer
 		runs = []
 
 		object.test_runs.each do |test_run|
-			runs.append({:id => test_run.id, :build_id => test_run.build_id})
+			runs.append({:id => test_run.id, :build_id => test_run.build_id, :url => url_for(controller: 'test_runs', action: 'show', id: test_run.id, :user_name => object[:name])})
 		end
 
 		runs
