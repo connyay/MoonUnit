1d145fd306619329fd31aa6aba01c7916b07277c
Jerrod <jllankfo@ncsu.edu>
2014-07-17 18:09:22 -0400

    Woops

--- server/app/models/test_run.rb
+++ server/app/models/test_run.rb
@@ -2,10 +2,4 @@ class TestRun < ActiveRecord::Base
   validates_presence_of :build_id, :user_id
   has_many :test_results, dependent: :destroy
   belongs_to :user
-
-  alias :old_test_results :test_results
-
-  def test_results
-  	return test_results[0]
-  end
 end
