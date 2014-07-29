8ef66c96e6ffb95b0a815dd08778d982b6b64fb6
Connor Hindley <conn.hindley@gmail.com>
2014-07-29 19:20:48 -0400

    take 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
-exec("cd ../ && npm run build", puts);
+exec("cd client && npm run build", puts);
