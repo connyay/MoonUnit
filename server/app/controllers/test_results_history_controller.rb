11e920518ced28ba71176641b4f7b090342c27eb
Jerrod <jllankfo@ncsu.edu>
2014-07-29 19:46:13 -0400

    Sort history

--- server/app/controllers/test_results_history_controller.rb
+++ server/app/controllers/test_results_history_controller.rb
@@ -3,7 +3,7 @@ class TestResultsHistoryController < ApplicationController
 	def show
 		test_result = TestResult.find(params[:test_result_id])
 		user = User.find_by(:name => params[:user_name])
-		results = TestResult.includes(:test_run, :test).where(:test_id => test_result.test_id, 'test_runs.user_id' => user.id)
+		results = TestResult.order('test_results.created_at DESC').includes(:test_run, :test).where(:test_id => test_result.test_id, 'test_runs.user_id' => user.id)
 		render :json => results, :status => :ok, :include_build_id => true
 	end
 
