ec4e6b1793b8095708ee7865c089911344ce2afc
Connor Hindley <conn.hindley@gmail.com>
2014-07-29 18:58:12 -0400

    take 2

--- client/build/script.js
+++ client/build/script.js
@@ -1,2 +1,4 @@
-var execSync = require('exec-sync');
-execSync('cd ../ && npm run build');
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec("cd ../ && npm run build", puts);
