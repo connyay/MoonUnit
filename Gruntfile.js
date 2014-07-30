17154d9cf5f1550832435a868d9fb2852b306a1b
Connor Hindley <conn.hindley@gmail.com>
2014-07-30 00:45:19 -0400

    use grunt-shell

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,30 @@
 module.exports = function(grunt) {
 
-grunt.initConfig({
-    execute: {
-        target: {
-            src: ['script.js']
+    grunt.initConfig({
+        shell: {      
+            npm: {        
+                command: function() {          
+                    grunt.log.writeln('Installing deps');          
+                    return 'cd client; npm install';        
+                }      
+            },
+            bower: {        
+                command: function() {          
+                    grunt.log.writeln('Installing bower');          
+                    return 'cd client; ./node_modules/gulp/bin/gulp.js bower';       
+                }      
+            },
+            build: {        
+                command: function() {          
+                    grunt.log.writeln('Running gulp');          
+                    return 'cd client; ./node_modules/gulp/bin/gulp.js build';        
+                }      
+            }    
         }
-    }
-})
+    })
 
-  grunt.loadNpmTasks('grunt-execute');
+    grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('default', ['execute']);
+    grunt.registerTask('default', ['shell:npm', 'shell:bower', 'shell:build']);
 
 };
